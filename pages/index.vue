614dd492b1096ba6602b78415f81b858aa7e955f
preginald <github.campout@simplelogin.com>
2023-04-22 19:27:52 +1000

    create an index home page

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Hello World!</h1>
+          <h2 class="subtitle">Welcome to my Nuxt.js project!</h2>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
