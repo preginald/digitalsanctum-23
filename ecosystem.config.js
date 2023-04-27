898416a325513f190b922160e786235543e18d1a
preginald <github.campout@simplelogin.com>
2023-04-27 18:22:05 +1000

    correct name and port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,10 @@ module.exports = {
     apps: [
         {
             name: 'digitalsanctum-23',
+            port: '3102',
             exec_mode: 'cluster',
             instances: 'max',
-            script: './.output/server/index.mjs',
-            port: 3102
+            script: './.output/server/index.mjs'
         }
     ]
 }
