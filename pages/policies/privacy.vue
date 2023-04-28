0dea5c6cc8990c36f05c38ad1e96b78294d24a7a
preginald <github.campout@simplelogin.com>
2023-04-29 08:24:23 +1000

    create privacy policy

---
+++ pages/policies/privacy.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container mx-auto px-4 py-8 prose lg:max-w-prose">
+        <h1 class="text-4xl font-bold mb-6">Privacy Policy</h1>
+
+        <h2 class="text-2xl font-semibold mb-3">1. Information Collection</h2>
+        <p class="mb-6">
+            We collect personal information (name, email, phone number), technical information (IP address, device type,
+            browser), and usage data (pages visited, time spent, actions taken) from users of our website or app.
+        </p>
+
+        <h2 class="text-2xl font-semibold mb-3">2. Use of Collected Information</h2>
+        <p class="mb-6">
+            We use the collected information to improve the user experience and site functionality, for marketing and
+            promotional purposes, to communicate with users (e.g., newsletters, customer support), and to comply with legal
+            requirements and prevent fraud.
+        </p>
+
+        <h2 class="text-2xl font-semibold mb-3">3. Sharing Information with Third Parties</h2>
+        <p class="mb-6">
+            We may share users' information with third parties in case of a business transfer (e.g., merger, acquisition)
+            and to comply with legal obligations or protect our rights.
+        </p>
+
+        <h2 class="text-2xl font-semibold mb-3">4. Retention of User Information</h2>
+        <p class="mb-6">
+            We retain users' information indefinitely, unless the user requests deletion.
+        </p>
+
+        <h2 class="text-2xl font-semibold mb-3">5. Cookies and Similar Technologies</h2>
+        <p class="mb-6">
+            We use cookies and similar tracking technologies on our website or app.
+        </p>
+
+        <h2 class="text-2xl font-semibold mb-3">6. Security</h2>
+        <p class="mb-6">
+            We ensure the security of users' information through secure socket layer (SSL) encryption, storing data on
+            secure servers, and conducting regular security audits and updates.
+        </p>
+
+        <h2 class="text-2xl font-semibold mb-3">7. Access, Update, and Deletion of Personal Information</h2>
+        <p>
+            Users can access and update their personal information through their account settings. To request deletion of
+            personal information, users must write to us.
+        </p>
+    </div>
+</template>
