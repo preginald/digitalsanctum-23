2cd119483a9f94b8ca2b23255e3d6ec7aad2516b
preginald <github.campout@simplelogin.com>
2023-05-02 21:50:52 +1000

    add styling

--- pages/policies/terms-of-use.vue
+++ pages/policies/terms-of-use.vue
@@ -2,16 +2,16 @@
     <div class="container mx-auto px-4 py-8 prose lg:max-w-prose">
         <h1 class="text-4xl font-bold mb-6">Terms of Use</h1>
 
-        <h2 class="mt-3">1. Introduction</h2>
-        <p>
+        <h2 class="text-2xl font-semibold mb-3">1. Introduction</h2>
+        <p class="mb-6">
             These Terms of Use ("Terms") govern your access to and use of the
             website, products, and services provided by Digital Sanctum ("we", "us",
             or "our"). By accessing or using our services, you agree to be bound by
             these Terms.
         </p>
 
-        <h2 class="mt-3">2. Scope of Services</h2>
-        <p>
+        <h2 class="text-2xl font-semibold mb-3">2. Scope of Services</h2>
+        <p class="mb-6">
             Digital Sanctum primarily serves small to medium-sized businesses
             seeking to optimise their workflow and leverage the latest IT and AI
             technologies for growth, improved efficiency and competitiveness. Our
@@ -19,32 +19,32 @@
             business process management, workflow automation, and IT support.
         </p>
 
-        <h2 class="mt-3">3. Payment and Fees</h2>
-        <p>
+        <h2 class="text-2xl font-semibold mb-3">3. Payment and Fees</h2>
+        <p class="mb-6">
             Fees for our services shall be payable as per the agreed-upon terms
             between Digital Sanctum and the client, which may include hourly rates,
             fixed project fees, or retainer arrangements. Invoices will be issued
             periodically, and payment is due within 30 days of the invoice date.
         </p>
 
-        <h2 class="mt-3">4. Modifications and Termination</h2>
-        <p>
+        <h2 class="text-2xl font-semibold mb-3">4. Modifications and Termination</h2>
+        <p class="mb-6">
             Digital Sanctum reserves the right to modify or discontinue, temporarily
             or permanently, our services with or without notice to you. You agree
             that Digital Sanctum shall not be liable to you or any third party for
             any modification, suspension, or discontinuance of our services.
         </p>
 
-        <h2 class="mt-3">5. Intellectual Property Rights</h2>
-        <p>
+        <h2 class="text-2xl font-semibold mb-3">5. Intellectual Property Rights</h2>
+        <p class="mb-6">
             Unless otherwise agreed in writing, all intellectual property rights,
             including copyrights, patents, and trade secrets, developed or created
             by Digital Sanctum during the course of providing services to you shall
             remain the property of Digital Sanctum.
         </p>
 
-        <h2 class="mt-3">6. Confidentiality</h2>
-        <p>
+        <h2 class="text-2xl font-semibold mb-3">6. Confidentiality</h2>
+        <p class="mb-6">
             Both parties agree to maintain the confidentiality of any proprietary or
             confidential information disclosed by one party to the other in
             connection with the provision of services under these Terms, except
@@ -52,16 +52,16 @@
             disclosing party.
         </p>
 
-        <h2 class="mt-3">7. Governing Law and Jurisdiction</h2>
-        <p>
+        <h2 class="text-2xl font-semibold mb-3">7. Governing Law and Jurisdiction</h2>
+        <p class="mb-6">
             These Terms shall be governed by and construed in accordance with the
             laws of Australia. The parties agree to submit to the exclusive
             jurisdiction of the courts of Australia for the resolution of any
             disputes arising from or relating to these Terms.
         </p>
 
-        <h2 class="mt-3">8. Limitation of Liability</h2>
-        <p>
+        <h2 class="text-2xl font-semibold mb-3">8. Limitation of Liability</h2>
+        <p class="mb-6">
             Digital Sanctum's liability for any claim arising out of or in
             connection with the provision of services under these terms, whether in
             contract, tort (including negligence) or otherwise, shall be limited to
