169c91b89886ca8724b0035dc0d8a3ae0767b79a
preginald <github.campout@simplelogin.com>
2023-05-02 21:50:04 +1000

    disable sticky nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,5 +1,6 @@
 <template>
-    <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+    <!-- <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600"> -->
+    <nav class="bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-600">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="/" class="flex items-center">
                 <!-- <img src="https://digitalsanctum.com.au/docs/images/logo.svg" class="h-8 mr-3" alt="Digital Sanctum Logo" /> -->
