357900d743b148b58074bdcfefb2333772746728
preginald <github.campout@simplelogin.com>
2023-05-25 20:26:25 +1000

    move footer to component

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,66 +3,20 @@
         <!-- Header and Navigation -->
         <Nav />
 
-
         <!-- Page Content -->
         <main class="main-content">
             <slot />
         </main>
+
         <!-- Footer -->
-        <footer class="bg-gray-800 py-12">
-            <div class="container mx-auto px-4">
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-8 text-white mb-8">
-                    <!-- Elevator Pitch -->
-                    <div class="flex flex-col md:col-span-2 lg:col-span-2">
-                        <h3 class="text-xl font-bold mb-4">Digital Sanctum</h3>
-                        <div class="flex space-x-4">
-                            <p>Digital Sanctum specialises in AI consulting and IT solutions for Boroondara businesses.
-                                Experience our expert guidance, tailored strategies, and empathetic approach to unlock your
-                                organisation's potential.</p>
-                        </div>
-                    </div>
-                    <!-- Company Information -->
-                    <!-- <div class="md:col-span-1 lg:col-span-1">
-                        <h3 class="text-xl font-bold mb-4 text-silver-light">Connect with us</h3>
-                        <p class="text-silver-light">55 Baker Ave</p>
-                        <p class="text-silver-light">Kew East VIC 3102</p>
-                        <p>Phone: (555) 123-4567</p>
-                        <p>Email: info@digitalsanctum.com</p>
-                    </div> -->
-
-                    <!-- Policies -->
-                    <div class="md:col-span-1 lg:col-span-1">
-                        <h5 class="text-sm font-bold mb-4 uppercase">Policies</h5>
-                        <ul class="space-y-2">
-                            <li>
-                                <NuxtLink to="/policies/privacy" class="text-blue-400 hover:text-blue-300">
-                                    Privacy
-                                </NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink to="/policies/terms-and-conditions" class="text-blue-400 hover:text-blue-300">
-                                    Terms & conditions
-                                </NuxtLink>
-                            </li>
-                            <!-- <li>
-                                <a href="#" class="text-blue-400 hover:text-blue-300">Contact Us</a>
-                            </li> -->
-                        </ul>
-                    </div>
-                </div>
-
-                <!-- Copyright -->
-                <div class="text-center text-white text-sm">
-                    <p class="text-silver-light">&copy; 2023 Digital Sanctum. All Rights Reserved.</p>
-                </div>
-            </div>
-        </footer>
+        <Footer />
 
     </div>
 </template>
 
 <script setup lang="ts">
 import Nav from '~/components/Nav.vue'
+import Footer from '~/components/Footer.vue';
 import { useSiteStore } from '~~/stores/siteStore';
 const siteStore = useSiteStore()
 
@@ -117,9 +71,4 @@ onBeforeUnmount(() => {
 .router-link-exact-active {
     color: white;
 }
-
-/* .sticky {
-    position: sticky;
-    top: 0;
-} */
 </style>
