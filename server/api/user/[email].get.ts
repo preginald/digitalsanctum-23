6e72110596712242e202afed9b8c7d444420df1b
preginald <github.campout@simplelogin.com>
2023-06-04 19:45:15 +1000

    Delete api

--- server/api/user/[email].get.ts
+++
@@ -1,7 +0,0 @@
-import { findUserByEmail } from "~~/server/database/repositories/userRepository";
-
-export default defineEventHandler(async (event) => {
-  const email = event.context.params.email;
-
-  return await findUserByEmail(email);
-});
