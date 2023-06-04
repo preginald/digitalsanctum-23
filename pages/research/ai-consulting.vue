644f511f883c2a89f09c98ff5f8841fd5b463448
preginald <github.campout@simplelogin.com>
2023-06-04 20:17:45 +1000

    Fix  radio input bug

--- pages/research/ai-consulting.vue
+++ pages/research/ai-consulting.vue
@@ -111,7 +111,8 @@
                     <template v-for="(option, index) in businessSizes" :key="index">
                         <div class="flex items-center">
                             <input @click="userStore.saveResearchByEmail()" :id="`business_size-radio-${index}`"
-                                type="radio" :value="option.value" v-model="userStore.user.business_size" name="budget"
+                                type="radio" :value="option.value" v-model="userStore.user.business_size"
+                                :name="`business_size-radio-${index}`"
                                 class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                             <label :for="`business_size-radio-${index}`"
                                 class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ option.text
