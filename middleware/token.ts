61a759f72eadab7f86b324506af58d0aae1079b3
preginald <github.campout@simplelogin.com>
2023-06-10 13:48:02 +1000

    move token logic to middleware

---
+++ middleware/token.ts
@@ -0,0 +1,20 @@
+import { useUserStore } from "~/stores/userStore"
+
+export default defineNuxtRouteMiddleware(async (to, from) => {
+    const userStore = useUserStore()
+    const router = useRouter()
+    var token
+
+    if (to.name === 'token-id') {
+        const id = to.params.id
+        token = await userStore.getTokenById(id)
+    }
+
+    if (token && token.status === "active") {
+        router.push(token.referrer);
+    } else {
+        console.log("Token not found or not active");
+        // You can perform any necessary actions or return an appropriate value
+        // For example, you can show an error message to the user or redirect to a default page
+    }
+})
--- pages/token/[id].vue
+++ pages/token/[id].vue
@@ -3,36 +3,9 @@
         <div class="mx-auto w-2/4 my-10">
         </div>
     </div>
-    <div>
-        {{ token }}
-    </div>
 </template>
 <script setup lang="ts">
 definePageMeta({
     middleware: ["token"]
 })
-
-import { useUserStore } from "~/stores/userStore"
-const userStore = useUserStore()
-const router = useRouter()
-const token = ref({ status: "", referrer: "" })
-
-const id = useRoute().params.id
-
-
-async function processToken() {
-    token.value = await userStore.getTokenById(id)
-    if (token.value && token.value.status === "active") {
-        router.push(token.value.referrer);
-    } else {
-        // Handle case when token is not found or not active
-        console.log("Token not found or not active");
-        // You can perform any necessary actions or return an appropriate value
-        // For example, you can show an error message to the user or redirect to a default page
-    }
-}
-
-onMounted(processToken)
-
-
 </script>
