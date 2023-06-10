6fac342dd55fccc653750d9cc59bcc3dfc80170a
preginald <github.campout@simplelogin.com>
2023-06-10 12:14:44 +1000

    Auto route from token link

--- pages/token/[id].vue
+++ pages/token/[id].vue
@@ -1,22 +1,6 @@
 <template>
     <div class="container mx-auto px-4">
         <div class="mx-auto w-2/4 my-10">
-            <div v-if="!token.status" class="relative z-0 w-full mb-6 group">
-                <label for="token" class="block text-gray-500 dark:text-gray-400">Please enter the security token</label>
-                <input type="email" name="email" id="email" v-model="id"
-                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600"
-                    placeholder="business email" required />
-                <button @click="processToken()" class="btn-primary">{{ tokenButton.text }}</button>
-            </div>
-            <div v-else-if="token.status == 'invalid'" class="relative z-0 w-full mb-6 group">
-                <h2>Invalid token!</h2>
-            </div>
-            <div v-else class="relative z-0 w-full mb-6 group">
-                <h2>The token has expired!</h2>
-                <button @click="router.push(token.referrer)" class="btn-primary">Click here to get fresh
-                    token</button>
-            </div>
-
         </div>
     </div>
     <div>
@@ -24,13 +8,17 @@
     </div>
 </template>
 <script setup lang="ts">
+definePageMeta({
+    middleware: ["token"]
+})
+
 import { useUserStore } from "~/stores/userStore"
 const userStore = useUserStore()
 const router = useRouter()
 const token = ref({ status: "", referrer: "" })
-const tokenButton = ref({ text: "Verify Token", class: "btn-primary" })
 
-const id = useRoute().params.id || ref("");
+const id = useRoute().params.id
+
 
 async function processToken() {
     token.value = await userStore.getTokenById(id)
@@ -43,5 +31,8 @@ async function processToken() {
         // For example, you can show an error message to the user or redirect to a default page
     }
 }
+
+onMounted(processToken)
+
 
 </script>
