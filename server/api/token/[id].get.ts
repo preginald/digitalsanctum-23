a06b87939eeaa51e621b132b92ca8ee4a3c37c58
preginald <github.campout@simplelogin.com>
2023-06-10 13:47:18 +1000

    remove debug log

--- server/api/token/[id].get.ts
+++ server/api/token/[id].get.ts
@@ -3,7 +3,5 @@ import { findToken } from "~/server/database/repositories/tokenRepository";
 export default defineEventHandler(async (event) => {
     const id = event.context.params.id
 
-    console.log(id)
-
     return await findToken(id);
 });
