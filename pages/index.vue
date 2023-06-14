80553739f5d123c26f73bf2146ed15fc2f5c9734
preginald <github.campout@simplelogin.com>
2023-06-15 09:20:37 +1000

    hide button while fixing bug

--- pages/index.vue
+++ pages/index.vue
@@ -189,7 +189,7 @@
             </p>
           </div>
         </div>
-        <NuxtLink to="/about/business-principles" class="btn-primary">Our Business Principles</NuxtLink>
+        <!-- <NuxtLink to="/about/business-principles" class="btn-primary">Our Business Principles</NuxtLink> -->
       </div>
     </section>
 
