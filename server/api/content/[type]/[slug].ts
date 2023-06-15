dfe531dee3729da687f1ef50058e6b0b6b50a2e3
preginald <github.campout@simplelogin.com>
2023-06-15 20:09:48 +1000

    new api to fetch articles

---
+++ server/api/content/[type]/[slug].ts
@@ -0,0 +1,9 @@
+import { findContentBySlug } from "~/server/database/repositories/contentRepositories";
+
+export default defineEventHandler(async (event) => {
+    const type = event.context.params.type;
+    const slug = event.context.params.slug;
+    // console.log(type, slug)
+    return await findContentBySlug(type, slug);
+});
+
