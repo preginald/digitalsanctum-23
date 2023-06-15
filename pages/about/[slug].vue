17382c0b95e1c08f8883beed118209847e65639a
preginald <github.campout@simplelogin.com>
2023-06-15 15:45:38 +1000

    new route for about business pages

--- pages/about/[slug].vue
+++ pages/about/[slug].vue
@@ -1,7 +1,10 @@
 <template>
     <main>
         <div class="container mx-auto px-4 py-8 prose lg:max-w-prose">
-            <ContentDoc />
+            {{ slug }}
         </div>
     </main>
 </template>
+<script setup lang="ts">
+const slug = useRoute().params.slug
+</script>
