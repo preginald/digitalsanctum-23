64e8b1a45a38d609e6c553aefc27b863a308ea0b
preginald <github.campout@simplelogin.com>
2023-06-17 22:40:50 +1000

    new component

---
+++ components/articles/Related.vue
@@ -0,0 +1,11 @@
+<template>
+    <h2>Related</h2>
+    <div v-for="article in related">
+        <NuxtLink :to="article.slug">{{ article.title }}</NuxtLink>
+    </div>
+</template>
+<script setup lang="ts">
+const tag = encodeURIComponent('AI Basics')
+console.log(tag)
+const { data: related } = await useFetch('/api/content/related/' + tag)
+</script>
