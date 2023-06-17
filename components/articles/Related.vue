af110282bd7ccd7471a0c5a9fcbc6a917617043e
preginald <github.campout@simplelogin.com>
2023-06-18 00:25:27 +1000

    filtered related list

--- components/articles/Related.vue
+++ components/articles/Related.vue
@@ -1,11 +1,15 @@
 <template>
-    <h2>Related</h2>
-    <div v-for="article in related">
-        <NuxtLink :to="article.slug">{{ article.title }}</NuxtLink>
+    <div v-if="contentStore.related.length">
+        <h2>Related</h2>
+        <ul>
+            <li v-for="article in contentStore.related">
+                <NuxtLink :to="article.slug">{{ article.title }}</NuxtLink>
+            </li>
+        </ul>
     </div>
 </template>
 <script setup lang="ts">
+import { useContentStore } from '~/stores/contentStore';
+const contentStore = useContentStore()
 const tag = encodeURIComponent('AI Basics')
-console.log(tag)
-const { data: related } = await useFetch('/api/content/related/' + tag)
 </script>
