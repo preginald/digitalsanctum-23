213cd1df67c845f0eb3e549c96b5af478e30ef6d
preginald <github.campout@simplelogin.com>
2023-06-22 09:11:41 +1000

    new page to create content

---
+++ pages/admin/content/create.vue
@@ -0,0 +1,23 @@
+<template>
+    <main class="container mx-auto px-4 pb-8">
+        <Create v-if="userStore.isAdmin()" />
+    </main>
+</template>
+<script setup lang="ts">
+import Create from '~/components/content/Create.vue'
+
+import { useUserStore } from '~/stores/userStore'
+const userStore = useUserStore()
+import { useContentStore } from '~/stores/contentStore';
+
+definePageMeta({
+    middleware: ["token"]
+})
+
+const contentStore = useContentStore()
+
+useHead({
+    title: contentStore.content.title
+})
+
+</script>
