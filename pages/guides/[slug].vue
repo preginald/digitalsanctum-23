10ab623f308e320f488f7b7ee30f042873754ed9
preginald <github.campout@simplelogin.com>
2023-06-22 09:10:15 +1000

    show editor if admin

--- pages/guides/[slug].vue
+++ pages/guides/[slug].vue
@@ -1,14 +1,18 @@
 <template>
     <div v-if="userStore.isAdmin()" class="mx-auto px-4 prose lg:max-w-prose">
-        <button class="btn-primary">Edit</button>
+        <button @click="toggleMode()" class="btn-primary">Edit</button>
     </div>
+    <main class="container mx-auto px-4 pb-8">
+        <Edit v-if="mode == 'edit'" />
+    </main>
     <main class="container mx-auto px-4 pb-8 prose lg:max-w-prose">
-        <Read />
+        <Read v-if="mode == 'read'" />
         <Related />
     </main>
 </template>
 <script setup lang="ts">
 import Read from '~/components/content/Read.vue'
+import Edit from '~/components/content/Edit.vue'
 import Related from '~/components/content/Related.vue';
 
 import { useUserStore } from '~/stores/userStore'
@@ -23,6 +27,12 @@ const contentStore = useContentStore()
 const type = "guides"
 const slug = "/api/content/" + type + "/" + useRoute().params.slug
 contentStore.getContentBySlug(slug, "AI Basics")
+const mode = ref("read")
+
+const toggleMode = () => {
+    mode.value = mode.value == "read" ? "edit" : "read"
+}
+
 useHead({
     title: contentStore.content.title
 })
