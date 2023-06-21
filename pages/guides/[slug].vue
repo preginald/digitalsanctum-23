3385f24d38e904ed24f90dc0b3161f7ed2436a9a
preginald <github.campout@simplelogin.com>
2023-06-21 11:27:15 +1000

    add button to edit content

--- pages/guides/[slug].vue
+++ pages/guides/[slug].vue
@@ -1,5 +1,9 @@
 <template>
-    <main class="container mx-auto px-4 py-8 prose lg:max-w-prose">
+    <div v-if="userStore.isAdmin()" class="mx-auto px-4 prose lg:max-w-prose">
+        <button class="btn-primary">Edit</button>
+    </div>
+    <main class="container mx-auto px-4 pb-8 prose lg:max-w-prose">
+        <!-- <div v-if="userStore.session && userStore.isAdmin()"> -->
         <Read />
         <Related />
     </main>
@@ -11,6 +15,10 @@ import Related from '~/components/content/Related.vue';
 import { useUserStore } from '~/stores/userStore'
 const userStore = useUserStore()
 import { useContentStore } from '~/stores/contentStore';
+
+definePageMeta({
+    middleware: ["token"]
+})
 
 const contentStore = useContentStore()
 const type = "guides"
