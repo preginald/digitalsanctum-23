9d3c7a73f8a38f516a8476237b938c437991329e
preginald <github.campout@simplelogin.com>
2023-06-22 18:36:27 +1000

    improve styling

--- pages/guides/index.vue
+++ pages/guides/index.vue
@@ -1,8 +1,9 @@
 <template>
     <main class="container mx-auto px-4 pb-8">
-        <h1>Guides</h1>
-        <NuxtLink :to="'/guides/' + content.slug" v-for="content in contentStore.contents">
-            <div class="py-5">
+        <h1 class="pl-5">Guides</h1>
+        <NuxtLink v-if="contentStore.contents.length" :to="'/guides/' + content.slug"
+            v-for="content in contentStore.contents">
+            <div class="p-5 dark:hover:bg-gray-800">
                 <h3 class="mb-1">{{ content.title }}</h3>
                 <span v-for="tag in content.tags"
                     class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">{{
