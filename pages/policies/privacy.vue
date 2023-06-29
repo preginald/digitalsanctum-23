62e5de23e038f51a4b13128b8fe411b09b8a7eaa
preginald <github.campout@simplelogin.com>
2023-06-29 23:20:57 +1000

    updated

--- pages/policies/privacy.vue
+++ pages/policies/privacy.vue
@@ -42,5 +42,42 @@
             Users can access and update their personal information through their account settings. To request deletion of
             personal information, users must write to us.
         </p>
+
+        <h2 class="text-2xl font-semibold mb-3">8. Children’s Privacy</h2>
+        <p>
+            Our services are not directed at children under the age of 13. We do not knowingly collect personal information
+            from children under 13. If you are a parent or guardian and believe your child has provided us with personal
+            information, please contact us.
+        </p>
+        <h2 class="text-2xl font-semibold mb-3">9. Third-Party Links</h2>
+        <p>
+            Our website or app may include links to other websites, apps or services whose privacy practices may differ from
+            ours. If you submit personal information to any of those websites, your information is governed by their privacy
+            policies.
+        </p>
+        <h2 class="text-2xl font-semibold mb-3">10. Changes to this Privacy Policy</h2>
+        <p>
+            We may update our Privacy Policy from time to time. Any changes will be posted on this page. If the changes are
+            significant, we will provide a more prominent notice (including, for certain services, email notification of
+            privacy policy changes).
+        </p>
+        <h2 class="text-2xl font-semibold mb-3">11. Your Rights and Responsibilities</h2>
+        <p>
+            As a user, you have certain rights regarding your personal information under applicable data protection laws.
+            This includes the right to access, correct, and delete your personal information, as well as the right to object
+            to or limit the use of your information. If you have any concerns or questions about how we handle your data,
+            please contact us.
+        </p>
+        <h2 class="text-2xl font-semibold mb-3">12. Contact Information</h2>
+        <p>
+            If you have any questions or comments about this Privacy Policy, or if you wish to exercise your rights related
+            to your personal information, please contact us at <a
+                href="mailto:hello@digitalsanctum.com.au">hello@digitalsanctum.com.au</a>.
+        </p>
+
+        <p>
+            Please note that this Privacy Policy does not apply to any information that you provide to third parties; you
+            should contact the third party directly for more information about their privacy practices.
+        </p>
     </div>
 </template>
