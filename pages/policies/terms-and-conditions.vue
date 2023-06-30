bdb34fef1a1b5526f013aa3e719af6cfd6f1f5be
preginald <github.campout@simplelogin.com>
2023-06-30 12:46:53 +1000

    updated document

--- pages/policies/terms-and-conditions.vue
+++ pages/policies/terms-and-conditions.vue
@@ -4,70 +4,118 @@
 
         <h2 class="text-2xl font-semibold mb-3">1. Introduction</h2>
         <p class="mb-6">
-            These Terms of Use ("Terms") govern your access to and use of the
-            website, products, and services provided by Digital Sanctum ("we", "us",
-            or "our"). By accessing or using our services, you agree to be bound by
+            These Terms of Use ("Terms") govern your access to and use of the website, products, and services ("Services")
+            provided by Digital Sanctum ("we", "us", or "our"). By accessing or using our Services, you agree to be bound by
             these Terms.
         </p>
 
         <h2 class="text-2xl font-semibold mb-3">2. Scope of Services</h2>
         <p class="mb-6">
-            Digital Sanctum primarily serves small to medium-sized businesses
-            seeking to optimise their workflow and leverage the latest IT and AI
-            technologies for growth, improved efficiency and competitiveness. Our
-            services include, but are not limited to, IT consulting, AI consulting,
-            business process management, workflow automation, and IT support.
+            Digital Sanctum primarily serves small to medium-sized businesses seeking to optimise their workflow and
+            leverage the latest IT and AI technologies for growth, improved efficiency, and competitiveness. Our Services
+            include, but are not limited to, IT consulting, AI consulting, business process management, workflow automation,
+            and IT support. A detailed outline of our Services is provided in a separate document or agreement.
         </p>
 
         <h2 class="text-2xl font-semibold mb-3">3. Payment and Fees</h2>
         <p class="mb-6">
-            Fees for our services shall be payable as per the agreed-upon terms
-            between Digital Sanctum and the client, which may include hourly rates,
-            fixed project fees, or retainer arrangements. Invoices will be issued
-            periodically, and payment is due within 30 days of the invoice date.
+            Fees for our Services shall be payable as per the agreed-upon terms between Digital Sanctum and the client,
+            which may include hourly rates, fixed project fees, or retainer arrangements. Invoices will be issued
+            periodically, and payment is due within 30 days of the invoice date. In the event of a late payment, Digital
+            Sanctum reserves the right to charge a late fee or cease provision of services until full payment is made.
         </p>
 
         <h2 class="text-2xl font-semibold mb-3">4. Modifications and Termination</h2>
         <p class="mb-6">
-            Digital Sanctum reserves the right to modify or discontinue, temporarily
-            or permanently, our services with or without notice to you. You agree
-            that Digital Sanctum shall not be liable to you or any third party for
-            any modification, suspension, or discontinuance of our services.
+            Digital Sanctum reserves the right to modify or discontinue, temporarily or permanently, our Services with or
+            without notice to you. You agree that Digital Sanctum shall not be liable to you or any third party for any
+            modification, suspension, or discontinuance of our Services.
         </p>
 
         <h2 class="text-2xl font-semibold mb-3">5. Intellectual Property Rights</h2>
         <p class="mb-6">
-            Unless otherwise agreed in writing, all intellectual property rights,
-            including copyrights, patents, and trade secrets, developed or created
-            by Digital Sanctum during the course of providing services to you shall
-            remain the property of Digital Sanctum.
+            Unless otherwise agreed in writing, all intellectual property rights, including copyrights, patents, and trade
+            secrets, developed or created by Digital Sanctum during the course of providing Services to you shall remain the
+            property of Digital Sanctum. Certain exceptions may apply where explicit arrangements have been made and agreed
+            upon in writing.
         </p>
 
         <h2 class="text-2xl font-semibold mb-3">6. Confidentiality</h2>
         <p class="mb-6">
-            Both parties agree to maintain the confidentiality of any proprietary or
-            confidential information disclosed by one party to the other in
-            connection with the provision of services under these Terms, except
-            where disclosure is required by law or with the written consent of the
-            disclosing party.
+            Both parties agree to maintain the confidentiality of any proprietary or confidential information disclosed by
+            one party to the other in connection with the provision of Services under these Terms, except where disclosure
+            is required by law or with the written consent of the disclosing party.
         </p>
 
         <h2 class="text-2xl font-semibold mb-3">7. Governing Law and Jurisdiction</h2>
         <p class="mb-6">
-            These Terms shall be governed by and construed in accordance with the
-            laws of Australia. The parties agree to submit to the exclusive
-            jurisdiction of the courts of Australia for the resolution of any
-            disputes arising from or relating to these Terms.
+            These Terms shall be governed by and construed in accordance with the laws of Australia. The parties agree to
+            submit to the exclusive jurisdiction of the courts of Australia for the resolution of any disputes arising from
+            or relating to these Terms.
         </p>
 
         <h2 class="text-2xl font-semibold mb-3">8. Limitation of Liability</h2>
         <p class="mb-6">
-            Digital Sanctum's liability for any claim arising out of or in
-            connection with the provision of services under these terms, whether in
-            contract, tort (including negligence) or otherwise, shall be limited to
-            the amount of fees paid by you for the specific service giving rise to
-            the claim, and Digital Sanctum shall not be liable for any indirect,
-            consequential, special or punitive damages.
+            Digital Sanctum's liability for any claim arising out of or in connection with the provision of Services under
+            these terms, whether in contract, tort (including negligence) or otherwise, shall be limited to the amount of
+            fees paid by you for the specific service giving rise to the claim, and Digital Sanctum shall not be liable for
+            any indirect, consequential, special or punitive damages.
         </p>
+
+        <h2 class="text-2xl font-semibold mb-3">9. Indemnity</h2>
+        <p class="mb-6">
+            You agree to indemnify and hold Digital Sanctum, its directors, officers, employees, agents, and affiliates
+            harmless from any and all claims, liabilities, damages, costs, and expenses, including legal fees, arising from
+            your use of our Services or your breach of these Terms.
+        </p>
+
+        <h2 class="text-2xl font-semibold mb-3">10. Warranties and Disclaimers</h2>
+        <p class="mb-6">
+            Digital Sanctum provides the Services "as is" and "as available" without warranty of any kind, either express or
+            implied, except as expressly provided to the contrary in writing. Digital Sanctum expressly disclaims any
+            warranties of merchantability, fitness for a particular purpose, or non-infringement.
+        </p>
+
+        <h2 class="text-2xl font-semibold mb-3">11. Force Majeure</h2>
+        <p class="mb-6">
+            Digital Sanctum shall not be liable for any delay or failure in performance under these Terms resulting from
+            acts beyond its reasonable control, including but not limited to acts of God, acts of war, epidemics, fire,
+            floods, telecommunication failures, power failures, internet failures, or governmental actions.
+        </p>
+
+        <h2 class="text-2xl font-semibold mb-3">12. Severability</h2>
+        <p class="mb-6">
+            If any provision of these Terms is found to be invalid or unenforceable by a court of competent jurisdiction,
+            such provision shall be severed from these Terms and the remaining provisions will remain in full force and
+            effect, continuing to be binding and enforceable.
+        </p>
+
+        <h2 class="text-2xl font-semibold mb-3">13. Entire Agreement</h2>
+        <p class="mb-6">
+            These Terms constitute the entire agreement between you and Digital Sanctum and supersede all prior and
+            contemporaneous understandings, agreements, representations, and warranties, both written and oral, regarding
+            the website, products, and Services.
+        </p>
+
+        <h2 class="text-2xl font-semibold mb-3">14. Changes to Terms</h2>
+        <p class="mb-6">
+            Digital Sanctum reserves the right, at its sole discretion, to modify or replace these Terms at any time. If a
+            revision is material, we will provide at least 30 days' notice prior to any new terms taking effect via the
+            email address provided by you, or by posting a notice on our website.
+        </p>
+
+        <h2 class="text-2xl font-semibold mb-3">15. Data Protection</h2>
+        <p class="mb-6">
+            Your use of our Services may involve the transfer, storage, and processing of your personal data. Please refer
+            to our Privacy Policy, which forms a part of these Terms, for information on how we collect, use, and protect
+            your personal data.
+        </p>
+
+        <h2 class="text-2xl font-semibold mb-3">16. Contact Information</h2>
+        <p class="mb-6">
+            If you have any questions about these Terms, please contact us at <a
+                href="mailto:hello@digitalsanctum.com.au">hello@digitalsanctum.com.au</a>.
+        </p>
+
     </div>
 </template>
